bbb932daf129a01a2f13f7b84114aba1c9432cd6
AntonGorbuno <joongleboo01@gmail.com>
2022-05-03 22:24:15 +0300

    Сделал:)

--- src/Main.java
+++ src/Main.java
@@ -24,15 +24,16 @@ public class Main {
         //3. Произвольный массив – тип и количество данных определите сами. Самостоятельно выберите способ создания массива: с помощью ключевого слова или сразу заполненный элементами.
         System.out.println("Задание 1.");
         int[] three = new int[3];
-        three[1] = 1;
-        three[2] = 2;
+        three[0] = 1;
+        three[1] = 2;
+        three[2] = 3;
         for (int i = 0; i < 3; i++) {
             System.out.println(three[i]);
         }
-        double[] drob = {0, 1.57, 7.654, 9.986};
-        drob[1] = 1.57;
-        drob[2] = 7.654;
-        drob[3] = 9.986;
+        double[] drob = {1.57, 7.654, 9.986};
+        drob[0] = 1.57;
+        drob[1] = 7.654;
+        drob[2] = 9.986;
         for (int i = 0; i < drob.length; i++) {
             System.out.println(drob[i]);
         }
@@ -43,19 +44,18 @@ public class Main {
         System.out.println(OP[1]);
         System.out.println("Задание №2");
         System.out.println(three[0] + ", " + three[1] + ", " + three[2]);
-        System.out.println(drob[0] + ", " + drob[1] + ", " + drob[2] + ", " + drob[3]);
+        System.out.println(drob[0] + ", " + drob[1] + ", " + drob[2]);
         System.out.println(OP[0] + ", " + OP[1]);
         System.out.println("Задание №3");
         System.out.println(three[2] + ", " + three[1] + ", " + three[0]);
-        System.out.println(drob[3] + ", " + drob[2] + ", " + drob[1] + ", " + drob[0]);
+        System.out.println(drob[2] + ", " + drob[1] + ", " + drob[0]);
         System.out.println(OP[1] + ", " + OP[0]);
         System.out.println("Задание №4");
         for (int i = 0; i < three.length; i++) {
-            if (three[i] % 2 == 0) {
-                System.out.print(three[i] + " ");
-            } else {
+            if (three[i] % 2 == 1){
                 three[i] = three[i] + 1;
             }
+            System.out.println(three[i]);
         }
 
     }
